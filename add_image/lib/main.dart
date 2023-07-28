265712bfb110a360be431d4dd78b52274ae217b4
harshraj1512 <harshprogram63@gmail.com>
2023-07-28 23:56:07 +0530

    changes

--- add_image/lib/main.dart
+++ add_image/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        // color
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
