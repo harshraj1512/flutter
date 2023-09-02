a7bca2300d2fae8ef40c11690f3775c5dd8f2c83
harshraj1512 <harshraj1512@gmail.com>
2023-09-02 22:07:10 +0530

    Second commit

---
+++ widget_button/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Flutter Conatainer'),
+      ),
+      // body: TextButton(
+      //   child: Text("Click Here"),
+      //   onPressed: () {
+      //     print("Button has been pressed");
+      //   },
+      //   onLongPress: (){
+      //     print("LongPressed");
+      //   },
+      // )
+      // body: ElevatedButton(
+      //   child: Text("Elevated"),
+      //   onPressed: () {
+      //     print("Pressed");
+      //   },
+      //   ),
+      body: OutlinedButton(
+        child: Text("Click Here"),
+        onPressed: () {
+          print("outline");
+        },),
+    );
+
+  }
+}
