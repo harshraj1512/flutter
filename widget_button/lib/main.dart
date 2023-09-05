e8b6d3343cdeab53ad81e0fbd3949a7642c72331
harshraj1512 <harshprogram63@gmail.com>
2023-09-06 00:28:15 +0530

    changes

--- widget_button/lib/main.dart
+++ widget_button/lib/main.dart
@@ -35,8 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
     
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
         title: Text('Flutter Conatainer'),
       ),
       // body: TextButton(
